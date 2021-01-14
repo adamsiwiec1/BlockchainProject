2c2771d267850ebfafc4c66ad767cd7f44b72a30
Adam Siwiec <acs0029@mix.wvu.edu>
2021-01-13 20:51:39 -0500

    Initial commit

---
+++ blockchain.py
@@ -0,0 +1,63 @@
+import hashlib
+import json
+from time import time
+
+class Blockchain(object):
+    def _init_(self):
+        self.chain = []
+        self.current_transactions = []
+
+        # Create the genesis block
+        self.new_block(previous_hash=1, proof=100)
+
+    def new_block(self, proof, previous_hash=None):
+        """
+        Create a new Block in the Blockchain
+        :param proof: <int> The proof given by the Proof of Work algorithm
+        :param previous_hash: (Optional) <str> Hash the previous Block
+        :return: <dict> New Block
+        """
+
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+    def new_transction(self, sender, recipient, amount):
+        #  Adds a new transaction to the list of transactions pass
+        """
+        :param sender: <str>
+        :param recipient:
+        :param amount:
+        :return:
+        """
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+
+        return self.last_block['index'] + 1
+
+    @property
+    def last_block(self):
+        # Returns the last Block in the chain pass
+        return self.chain[-1]
+
+    @staticmethod
+    def hash(block):
+        """
+        Creates a SHA-256 hash of a Block
+        :param block: <dict> Block
+        :return: <str>
+        """
+
+        # We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdisgest()
+
+
+
