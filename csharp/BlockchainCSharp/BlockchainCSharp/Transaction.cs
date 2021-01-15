d6004b541948f73f5b724864858328df498b1c8c
Adam Siwiec <acs0029@mix.wvu.edu>
2021-01-15 02:17:37 -0500

    added csharp

---
+++ csharp/BlockchainCSharp/BlockchainCSharp/Transaction.cs
@@ -0,0 +1,10 @@
+﻿
+namespace BlockChainDemo
+{
+    public class Transaction
+    {
+        public int Amount { get; set; }
+        public string Recipient { get; set; }
+        public string Sender { get; set; }
+    }
+}
